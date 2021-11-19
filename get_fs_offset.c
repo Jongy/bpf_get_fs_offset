85bbdb3d3b54406944a0f6d8c77117e4d4a35f3e
Yonatan Goldschmidt <yon.goldschmidt@gmail.com>
2021-11-20 01:57:54 +0200

    Exit with error if not found

--- get_fs_offset.c
+++ get_fs_offset.c
@@ -95,15 +95,18 @@ int main(int argc, const char *argv[]) {
         break;
 
     case STATUS_ERROR:
-        printf("had an error: %d\n", (int)output.offset);
+        fprintf(stderr, "had an error: %d\n", (int)output.offset);
+        err = 1;
         break;
 
     case STATUS_NOTFOUND:
-        printf("fs not found, searched for %u bytes into task_struct\n", MAX_TASK_STRUCT);
+        fprintf(stderr, "fs not found, searched for %u bytes into task_struct\n", MAX_TASK_STRUCT);
+        err = 1;
         break;
 
     case STATUS_DUP:
-        printf("found multiple matching offsets!\n");
+        fprintf(stderr, "found multiple matching offsets!\n");
+        err = 1;
         break;
     }
 
